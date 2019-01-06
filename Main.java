705d1c8d0fc0b055ebfc0211af18158ec82a48ac
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2019-01-05 23:47:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         int[] arr = new int[]{45, 56, 77, 24, 12, 98};
-        int max = arr[0]; // O(1)
+        int max = arr[0];
         for(int i = 1; i < arr.length; i++)
         {
             if(arr[i] > max)
